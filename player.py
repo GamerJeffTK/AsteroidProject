d3910a5658da1e33fcb7ae1b5f0f2a303361457a
GamerJeff <gamerjefftk@gmail.com>
2025-03-10 13:30:08 -0400

    updated classes into groups

--- player.py
+++ player.py
@@ -29,6 +29,7 @@ class Player(CircleShape):
 
 	def draw(self, screen):
 		pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
 	# in the player class
 	def triangle(self):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
